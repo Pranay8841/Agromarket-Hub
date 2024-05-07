ac81a4a08e5c1cb5756cb2cf5201f7eecfed79ac
PRANAY BHANDEKAR <97648682+Pranay8841@users.noreply.github.com>
2024-05-07 17:05:16 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://agromarket-hub.onrender.com",
+    origin: "https://agromarket-hub.onrender.com/api/v1",
     credentials: true,
   })
 )
